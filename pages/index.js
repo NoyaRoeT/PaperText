55b102e7a5b7d1482f39d4fea9a31e1250410654
kenny019 <ngkenny019@gmail.com>
2022-12-15 21:51:55 +0800

    Setup eslint and prettier

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-      </Head>
-      <div>
-        <h1 className='text-blue-500'>
-          Hello World!
-        </h1>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Head></Head>
+			<div>
+				<h1 className="text-blue-500">Hello World!</h1>
+			</div>
+		</>
+	);
 }
