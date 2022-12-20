3da14d51bfaaec438835c212cbf36b47d13031c3
kenny019 <ngkenny019@gmail.com>
2022-12-20 21:24:45 +0800

    Added tags and genre pages skeleton

---
+++ pages/genres/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Genres() {
+    return (
+        <>
+            <Head>
+                <title>PaperText | Genres</title>
+            </Head>
+            <main className="max-w-screen-xl mx-auto space-y-4 mt-10 mb-20"></main>
+        </>
+    );
+}
---
+++ pages/tags/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Tags() {
+    return (
+        <>
+            <Head>
+                <title>PaperText | Tags</title>
+            </Head>
+            <main className="max-w-screen-xl mx-auto space-y-4 mt-10 mb-20"></main>
+        </>
+    );
+}
