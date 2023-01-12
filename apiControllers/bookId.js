e6d621f9de5abebee2245fd3d2d40c6e9a8e082d
NoyaReoT <rayonteo@gmail.com>
2023-01-12 21:27:28 +0800

    Added /api/books/[bookId] endpt

---
+++ apiControllers/bookId.js
@@ -0,0 +1,28 @@
+import getServerSideSession from "../helpers/auth/getServerSideSession";
+import supabase from "../helpers/supabase";
+
+const handleGetBookId = async (req, res) => {
+    const { book_id } = req.query;
+    const { data, error, status } = await supabase.rpc("get_book_info", {
+        bk_id: book_id,
+    });
+
+    if (error) {
+        res.status(status).json({ error });
+        return;
+    }
+
+    res.status(status).json({ data });
+};
+
+const handlePostBookId = async (req, res) => {
+    res.send("in progress");
+};
+
+const handlePutBookId = async (req, res) => {
+    res.send("in progress");
+};
+
+const handleDeleteBookId = async (req, res) => {
+    res.send("in progress");
+};
